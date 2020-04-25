a8d6e1eb0a393ce677f402b76f8d7d6eaf6981c6
Matthieu <matthieu.jeanson@gmail.com>
2020-04-25 13:34:28 -0700

    rename project

--- bg_upgrades/wsgi.py
+++ bg_upgrades/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tuckbox_pattern project.
+WSGI config for bg_upgrades project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tuckbox_pattern.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bg_upgrades.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tuckbox_pattern.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bg_upgrades.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

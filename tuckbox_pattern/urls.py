f9f1167b9af8d15156fb803e1e26ebc0c081171e
Matthieu <matthieu.jeanson@gmail.com>
2020-04-25 12:20:06 -0700

    rename app from main to tuckbox

--- tuckbox_pattern/urls.py
+++ tuckbox_pattern/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('tuck/', include('main.urls')),
+    path('tuck/', include('tuckbox.urls')),
     path('admin/', admin.site.urls),
 ]

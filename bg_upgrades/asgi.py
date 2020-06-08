ecc70dffdd705644def52b5df88503db328a79fa
Matthieu <matthieu.jeanson@gmail.com>
2020-06-07 21:24:05 -0700

    Try to change some configurations for heroku

--- bg_upgrades/asgi.py
+++ bg_upgrades/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tuckbox_pattern project.
+ASGI config for bg_upgrades project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tuckbox_pattern.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bg_upgrades.settings')
 
 application = get_asgi_application()

72babc2b858a8578e48fac36ce753317fc94c4c8
Matthieu <matthieu.jeanson@gmail.com>
2020-06-11 23:53:05 -0700

    Add Procfile for Heroku Add gunicornd Add heroku http host as a valid host

--- bg_upgrades/settings.py
+++ bg_upgrades/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'to2a5#&-dgyxi3x7))!oy+!vo7n)h7b_p=con0glby^vhqce=('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["bg-upgrades.herokuapp.com", "127.0.0.1"]
 

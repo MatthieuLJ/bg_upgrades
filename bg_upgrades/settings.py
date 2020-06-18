8fa596aa908a2d2c4f168137e1c658ab948b7b81
Matthieu <matthieu.jeanson@gmail.com>
2020-06-18 14:29:56 -0700

    WIP, start following instructions to add celery

--- bg_upgrades/settings.py
+++ bg_upgrades/settings.py
@@ -135,3 +135,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'tuckbox/static'),
 )
+
+# Celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
